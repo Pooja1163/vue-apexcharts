f068b37c760606e69a8317c9c16a565ea25f4415
Pooja1163 <105694926+Pooja1163@users.noreply.github.com>
2022-09-12 16:44:18 +0530

    Update AreaExample.vue

--- example/src/charts/AreaExample.vue
+++ example/src/charts/AreaExample.vue
@@ -31,9 +31,6 @@ export default {
       series: [{
           name: 'series1',
           data: [31, 40, 28, 51, 42, 109, 100]
-      }, {
-          name: 'series2',
-          data: [11, 32, 45, 32, 34, 52, 41]
       }],
     }
   },
